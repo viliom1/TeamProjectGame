17e6a97294843c7b31c5f9bc4c00d6028929fb31
unknown <bogdan_kz@yahoo.com>
2015-09-30 08:53:54 +0300

    Signed-off-by: unknown <bogdan_kz@yahoo.com>

--- CoinCollector/Program.cs
+++ CoinCollector/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
+ 
+ 
 namespace CoinCollector
 {
     class Program
@@ -15,29 +15,30 @@ namespace CoinCollector
             int wallHits = 0; // wallhits tracking
             int steps = 0; // steps tracking
             bool isCoin = false;
-            string[,] matrix = new string[20, 20];
-            int[] position = { 0, 0 }; // position of the "@" in the array
+            string[,] matrix = new string[22, 22];
+            int[] position = { 1, 1 }; // position of the "@" in the array
             matrix = BoardGen(matrix);
             string[,] board = WallGenerator(matrix);
             board = CoinGen(board);
-            int[] consolePos = { 1, 1 }; // position of the "@" on the console
-            
+            int[] consolePos = { 3, 2 }; // position of the "@" on the console
+ 
             BoardPrint(board, points, wallHits, steps);
             ColorCoinsAndWalls(board);
+            BoardCorrection(board);
             while (true)
             {
                 ConsoleKeyInfo movement = Console.ReadKey(); // reading the key
                 // now checking what key was read
-                if (movement.Key == ConsoleKey.RightArrow) 
+                if (movement.Key == ConsoleKey.RightArrow)
                 {
-                    bool wall = IsWallRight(board,position);
+                    bool wall = IsWallRight(board, position);
                     StepsUpdate(steps);
                     steps++;
                     if (wall == true)
                     {
                         WallHitsUpdate(wallHits);
                         wallHits++;
-                        
+ 
                     }
                     else
                     {
@@ -48,15 +49,15 @@ namespace CoinCollector
                             CoinCollected(points);
                             points += 100;
                         }
-
+ 
                         MoveRight(position, consolePos);
-
+ 
                         isCoin = false;
                     }
                 }
                 else if (movement.Key == ConsoleKey.LeftArrow)
                 {
-                    
+ 
                     bool wall = IsWallLeft(board, position);
                     StepsUpdate(steps);
                     if (wall == true)
@@ -73,10 +74,10 @@ namespace CoinCollector
                             CoinCollected(points);
                             points += 100;
                         }
-
-
-                        MoveLeft(position,consolePos);
-                        
+ 
+ 
+                        MoveLeft(position, consolePos);
+ 
                     }
                 }
                 else if (movement.Key == ConsoleKey.DownArrow)
@@ -89,7 +90,7 @@ namespace CoinCollector
                         WallHitsUpdate(wallHits);
                         wallHits++;
                     }
-                    
+ 
                     else
                     {
                         isCoin = IsCoinDown(board, position);
@@ -99,10 +100,10 @@ namespace CoinCollector
                             CoinCollected(points);
                             points += 100; ;
                         }
-                    
-                    
-                        MoveDown(position,consolePos);
-                        
+ 
+ 
+                        MoveDown(position, consolePos);
+ 
                     }
                 }
                 else if (movement.Key == ConsoleKey.UpArrow)
@@ -124,10 +125,10 @@ namespace CoinCollector
                             CoinCollected(points);
                             points += 100;
                         }
-
-
-                        MoveUp(position,consolePos);
-                        
+ 
+ 
+                        MoveUp(position, consolePos);
+ 
                     }
                 }
                 if (points == 500)
@@ -135,26 +136,36 @@ namespace CoinCollector
                     break;
                 }
             }
-            
+ 
+            Console.WriteLine();
             int finalScore = points - wallHits * 10 - steps; // calculating the final score
-            if (finalScore >0)
+            if (finalScore > 0)
             {
-                Console.WriteLine("GEEEEGEEEEE!!!!!! Your score is: {0}",finalScore);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("GEEEEGEEEEE!!!!!! Your score is: {0}", finalScore);
             }
-            else if (finalScore<0)
+            else if (finalScore < 0)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("YOU SUCK !!! Your score is: {0}", finalScore);
             }
             else
             {
                 Console.WriteLine("GAME OVER !!! Your score is 0");
             }
-
+ 
             Console.ReadKey();
         }
-        static void BoardPrint(string[,] matrix,int points,int wallHits,int steps)
+        static void BoardPrint(string[,] matrix, int points, int wallHits, int steps)
         {
-            Console.WriteLine("Points:{0}   Hits:{1}   Steps:{2}", points, wallHits, steps);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Points:{0}", points);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("   Hits:{0}",wallHits);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("   Steps:{0}",steps);
+            Console.ResetColor();
+            Console.WriteLine();
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -169,9 +180,9 @@ namespace CoinCollector
             Random generator = new Random();
             for (int i = 0; i < 5; i++)
             {
-                
-                int rndRow = generator.Next(1,20);
-                int rndCol = generator.Next(0, 20);
+ 
+                int rndRow = generator.Next(1, 18);
+                int rndCol = generator.Next(1, 18);
                 if (matrix[rndRow, rndCol] == "$")
                 {
                     matrix[rndCol, rndRow] = "$";
@@ -185,23 +196,35 @@ namespace CoinCollector
         }
         static string[,] BoardGen(string[,] matrix)
         {
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 22; i++)
             {
-                for (int j = 0; j < 20; j++)
+                for (int j = 0; j < 22; j++)
                 {
-                    if (i == 0 && j == 0)
+                    if (i == 1 && j == 1)
                     {
                         matrix[i, j] = "@";
                     }
                     else
                     {
-                        matrix[i, j] = ".";
+                        if (i == 0 || i == 21)
+                        {
+                            matrix[i, j] = "_";
+                        }
+                        else if (j == 0 || j == 21)
+                        {
+                            matrix[i, j] = "|";
+                        }
+                        else
+                        {
+                            matrix[i, j] = ".";
+                        }
+ 
                     }
                 }
             }
             return matrix;
         }
-        static void MoveRight(int[]position, int[] consolePos)
+        static void MoveRight(int[] position, int[] consolePos)
         {
             Console.SetCursorPosition(consolePos[0], consolePos[1]); // setting the cursor to the current position of the "@"
             Console.Write(". ");
@@ -211,11 +234,11 @@ namespace CoinCollector
             Console.SetCursorPosition(0, 22); // moving the cursor out of the board
             position[1]++; // updating the position
             consolePos[0] += 2;// and the console position
-            
+ 
         }   // Move**** are pringting on the console with the movement you want
         static void MoveLeft(int[] position, int[] consolePos)
         {
-            Console.SetCursorPosition(consolePos[0]-2, consolePos[1]);
+            Console.SetCursorPosition(consolePos[0] - 2, consolePos[1]);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("@");
             Console.ResetColor();
@@ -223,13 +246,13 @@ namespace CoinCollector
             Console.SetCursorPosition(0, 22);
             position[1]--;
             consolePos[0] -= 2;
-        }  
+        }
         static void MoveDown(int[] position, int[] consolePos)
         {
             Console.SetCursorPosition(consolePos[0], consolePos[1]);
             Console.Write(".");
-            Console.SetCursorPosition(consolePos[0], consolePos[1]+1);
-            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.SetCursorPosition(consolePos[0], consolePos[1] + 1);
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("@");
             Console.ResetColor();
             Console.SetCursorPosition(0, 22);
@@ -240,59 +263,59 @@ namespace CoinCollector
         {
             Console.SetCursorPosition(consolePos[0], consolePos[1]);
             Console.Write(".");
-            Console.SetCursorPosition(consolePos[0], consolePos[1] -1);
+            Console.SetCursorPosition(consolePos[0], consolePos[1] - 1);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("@");
             Console.ResetColor();
             Console.SetCursorPosition(0, 22);
             position[0]--;
-            consolePos[1]--; 
+            consolePos[1]--;
         }
-        static bool IsCoinRight(string[,]matrix,int[] positon)
+        static bool IsCoinRight(string[,] matrix, int[] positon)
         {
             bool coin = false;
-            if (matrix[positon[0],positon[1]+1] == "$")
+            if (matrix[positon[0], positon[1] + 1] == "$")
             {
                 matrix[positon[0], positon[1] + 1] = ".";
                 coin = true;
             }
-            
+ 
             return coin;
         }  //IsCoin*** checks if the next movement lands on a coin
-        static bool IsCoinLeft (string[,]matrix,int[] positon)
+        static bool IsCoinLeft(string[,] matrix, int[] positon)
         {
             bool coin = false;
-            if (matrix[positon[0], positon[1] -1] == "$")
+            if (matrix[positon[0], positon[1] - 1] == "$")
             {
                 matrix[positon[0], positon[1] - 1] = ".";
                 coin = true;
             }
             return coin;
         }
-        static bool IsCoinDown (string[,]matrix,int[] positon)
+        static bool IsCoinDown(string[,] matrix, int[] positon)
         {
             bool coin = false;
-            if (matrix[positon[0]+1, positon[1]] == "$")
+            if (matrix[positon[0] + 1, positon[1]] == "$")
             {
                 matrix[positon[0] + 1, positon[1]] = ".";
                 coin = true;
             }
             return coin;
         }
-        static bool IsCoinUp (string[,]matrix,int[] positon)
+        static bool IsCoinUp(string[,] matrix, int[] positon)
         {
             bool coin = false;
-            if (matrix[positon[0]-1, positon[1]] == "$")
+            if (matrix[positon[0] - 1, positon[1]] == "$")
             {
                 matrix[positon[0] - 1, positon[1]] = ".";
                 coin = true;
             }
             return coin;
         }
-        static bool IsWallRight(string[,]matrix,int[] position)
+        static bool IsWallRight(string[,] matrix, int[] position)
         {
             bool wall = false;
-            if (matrix.GetLength(0) == position[1]+1 || matrix[position[0],position[1]+1] == "|")
+            if (matrix.GetLength(0) == position[1] + 1 || matrix[position[0], position[1] + 1] == "|")
             {
                 wall = true;
             }
@@ -301,39 +324,41 @@ namespace CoinCollector
         static bool IsWallLeft(string[,] matrix, int[] position)
         {
             bool wall = false;
-            if (0 > position[1] - 1 || matrix[position[0],position[1]-1] == "|")
+            if (0 > position[1] - 1 || matrix[position[0], position[1] - 1] == "|")
             {
                 wall = true;
             }
             return wall;
         }
-        static bool IsWallDown(string[,]matrix,int[] position)
+        static bool IsWallDown(string[,] matrix, int[] position)
         {
             bool wall = false;
-            if (matrix.GetLength(0) == position[0] + 1 || matrix[position[0]+1,position[1]] == "|")
+            if (matrix[position[0] + 1, position[1]] == "_" || matrix[position[0] + 1, position[1]] == "|")
             {
                 wall = true;
             }
+ 
+ 
             return wall;
         }
-        static bool IsWallUp(string[,]matrix,int[] position)
+        static bool IsWallUp(string[,] matrix, int[] position)
         {
             bool wall = false;
-            if (0> position[0] - 1 || matrix[position[0]-1,position[1]] == "|")
+            if (matrix[position[0] - 1, position[1]] == "_" || matrix[position[0] - 1, position[1]] == "|")
             {
                 wall = true;
             }
             return wall;
         }
-        static string [,] WallGenerator (string[,] matrix)
+        static string[,] WallGenerator(string[,] matrix)
         {
-            
+ 
             Random generator = new Random();
             for (int i = 0; i < 15; i++)
             {
-
-                int rndRow = generator.Next(1, 20);
-                int rndCol = generator.Next(0, 20);
+ 
+                int rndRow = generator.Next(1, 17);
+                int rndCol = generator.Next(1, 17);
                 if (matrix[rndRow, rndCol] == "$" || matrix[rndRow, rndCol] == "|")
                 {
                     matrix[rndRow + 1, rndCol + 1] = "|";
@@ -344,42 +369,48 @@ namespace CoinCollector
                 }
             }
             return matrix;
-
+ 
         }    // generates walls on the board in random positions
-        static void CoinCollected (int points)
+        static void CoinCollected(int points)
         {
             points += 100;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.SetCursorPosition(7, 0);
             Console.Write(points);
+            Console.ResetColor();
         }  // updates the score
-        static void StepsUpdate (int steps)
+        static void StepsUpdate(int steps)
         {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             steps++;
             Console.SetCursorPosition(26, 0);
             Console.Write(steps);
+            Console.ResetColor();
         } // updates the steps
         static void WallHitsUpdate(int wallHits)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             wallHits++;
             Console.SetCursorPosition(16, 0);
             Console.Write(wallHits);
             Console.SetCursorPosition(0, 22);
+            Console.ResetColor();
         } //updates the hits
-        static void ColorCoinsAndWalls (string[,] board)
+        static void ColorCoinsAndWalls(string[,] board)
         {
             for (int i = 0; i < board.GetLength(0); i++)
             {
                 for (int j = 0; j < board.GetLength(1); j++)
                 {
-                    if (board[i,j] == "$")
+                    if (board[i, j] == "$")
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.SetCursorPosition(j*2+1,i+1);
+                        Console.SetCursorPosition(j * 2 + 1, i + 1);
                         Console.Write("$");
                         Console.ResetColor();
                         Console.SetCursorPosition(0, 22);
                     }
-                    else if(board[i,j] == "|")
+                    else if (board[i, j] == "|")
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.SetCursorPosition(j * 2 + 1, i + 1);
@@ -387,7 +418,16 @@ namespace CoinCollector
                         Console.ResetColor();
                         Console.SetCursorPosition(0, 22);
                     }
-                    else if(board[i,j] == "@")
+                    else if (board[i, j] == "_")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.SetCursorPosition(j * 2 + 1, i + 1);
+                        Console.Write("_");
+                        Console.ResetColor();
+                        Console.SetCursorPosition(0, 22);
+                    }
+ 
+                    else if (board[i, j] == "@")
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.SetCursorPosition(j * 2 + 1, i + 1);
@@ -395,8 +435,26 @@ namespace CoinCollector
                         Console.ResetColor();
                         Console.SetCursorPosition(0, 22);
                     }
-
+ 
                 }
+            }
+        }
+        static void BoardCorrection (string[,]board)
+        {
+            for (int i = 1; i < board.GetLength(0)-1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(0, i + 1);
+                Console.Write(" | ");
+ 
+            }
+            for (int i = 1; i < board.GetLength(1)*2; i++)
+            {
+                //Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(i, 1);
+                Console.Write("_");
+                Console.SetCursorPosition(i, 22);
+                Console.Write("_");
             }
         }
     }
